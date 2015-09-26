738d3ecf8b5f0df571c569cc9cd4b9c2ca7c6d48
Jeffrey Balies <jeffbailes@gmail.com>
2015-09-26 16:07:12 +1000

    Made it so that it's pretty much done now.

--- run_analysis.R
+++ run_analysis.R
@@ -1,11 +1,19 @@
 # The column names are in 'features.txt'.
 col_names <- read.table("UCI HAR Dataset/features.txt")[,2]
+# Column names we want to keep at the end: means or standard deviations.
+# The only one's we're interested in are ones that end with either
+# - mean(); or
+# - std().
+keep_cols <- grepl("-mean\\(\\)", col_names) | grepl("-std\\(\\)", col_names)
+# Modify col_names to be a bit nicer (get rid of , () and -).
+col_names <- gsub('\\(', '_', gsub('\\)', '', gsub(',', '.', gsub('\\(\\)', '', gsub('-', '_', col_names)))))
+
 # Read in the training and test data.
 data <- read.table("UCI HAR Dataset/train/X_train.txt", col.names=col_names)
 # Append the test data to the train data.
 data <- rbind(data, read.table("UCI HAR Dataset/test/X_test.txt", col.names=col_names))
-# Filter out the columns that aren't means or standard deviations.
-keep_cols <- grepl("-mean\\(\\)", col_names) | grepl("-std\\(\\)", col_names)
+
+# Filter out the columns we don't want.
 data <- data[keep_cols]
 
 # Also read in the activity data.
@@ -22,3 +30,14 @@ subject <- rbind(read.table("UCI HAR Dataset/train/subject_train.txt"),
 
 data <- cbind(subject, activity, data)
 names(data)[1:2] <- c("Subject", "Activity")
+
+# Use dplyr to summarise each variable, grouped by subject and activity
+# by taking it's mean.
+library(dplyr)
+averages <- (data
+             %>% group_by(Subject, Activity)
+             %>% summarise_each(funs(mean))
+             )
+
+# Write the output to a file.
+write.table(averages, file="averages.txt", row.name=FALSE)
